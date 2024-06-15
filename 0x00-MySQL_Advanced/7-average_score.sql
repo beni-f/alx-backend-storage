222197f49223a01fa829996c008786c83c94203b
beni-f <benifissha@gmail.com>
2024-06-15 18:44:17 +0300

    updated

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,25 +2,18 @@
 
 DELIMITER $$
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser (
     IN user_id INT
 )
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE num_corrections INT;
-    DECLARE avg_score FLOAT;
-
-    SELECT SUM(score), COUNT(*) INTO total_score, num_corrections
-    FROM corrections
-    WHERE user_id = user_id;
-
-    IF num_corrections > 0 THEN
-        SET  avg_score = total_score / num_corrections - 3;
-    END IF;
-
     UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+    SET average_score = (
+        SELECT AVG(score)
+        FROM corrections
+        WHERE corrections.user_id = user_id
+    )
+    WHERE id = user_id
 END$$
 
 DELIMITER ;
