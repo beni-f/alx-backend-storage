305898b0d4620a9a0f3b96c575e1204375df7fcd
beni-f <benifissha@gmail.com>
2024-06-16 14:15:36 +0300

    updated

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -2,4 +2,5 @@
 CREATE VIEW need_meeting as 
 SELECT name 
 FROM students
-WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < ) 
+WHERE score < 80 
+    AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
