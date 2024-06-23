599afa9f1143d8026f92dcc41dbeb6128a344a42
beni-f <benifissha@gmail.com>
2024-06-23 19:08:05 +0300

    9-insert_school

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,5 +5,8 @@
 
 
 def insert_school(mongo_collection, **kwargs):
+    """
+        Inserts a new document in a collection based on kwargs
+    """
     result = mongo_collection.insertOne(kwargs)
     return result._id
